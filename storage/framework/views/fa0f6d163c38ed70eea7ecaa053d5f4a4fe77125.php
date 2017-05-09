4eb7b044ad1e8301da93a7d26ffbf7eb06c58b9c
emil.cerezo <emilio.cerezo@republisys.com>
2017-05-09 16:00:04 +0800

    additional

---
+++ storage/framework/views/fa0f6d163c38ed70eea7ecaa053d5f4a4fe77125.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilio.cerezo
+ * Date: 5/8/17
+ * Time: 8:50 AM
+ */
+?>
+
+<!DOCTYPE html>
+
+<html lang="<?php echo e(config('app.locale')); ?>">
+
+    <head>
+
+        <meta charset="utf-8">
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>" />
+
+        <title>dPort - <?php echo $__env->yieldContent('title'); ?></title>
+
+    </head>
+
+    <body>
+        <input id="BASE_URL" type="hidden" value="<?php echo e(URL::to('/')); ?>"/>
+
+        <div class="container">
+
+            <?php echo $__env->yieldContent('content'); ?>
+
+        </div>
+    </body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,9 @@ const { mix } = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
 
+    
+
+    /* Plugins */
     .styles([
         'resources/assets/sass/plugins/materialize.css'
     ], 'public/css/plugins.css')
@@ -21,3 +24,5 @@ mix.js('resources/assets/js/app.js', 'public/js')
     .scripts ([
         'resources/assets/js/plugins/materialize.js'
     ], 'public/js/plugins.js');
+
+
