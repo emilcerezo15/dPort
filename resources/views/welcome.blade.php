4135b7d7c9e7ba25a1b2e00f16d6aaf082b0b658
emil.cerezo <emilio.cerezo@republisys.com>
2017-05-11 15:59:42 +0800

    animate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,119 +3,59 @@
 @section('title', 'Home')
 
 @section('content')
-<div class="row">
-    <div class="col s12 main">
-        <div class="bg think show"></div>
-        <div class="bg enter  "></div>
-        <div class="bg-filter"></div>
-        <nav>
-            <div class="nav-list">
-                <div><a href=""><i class="fa fa-user"></i></a></div>
-                <div><a href=""><i class="fa fa-desktop"></i></a></div>
-                <div><a href=""><i class="fa fa-mouse-pointer"></i></a></div>
-                <div><a href=""><i class="fa fa-envelope"></i></a></div>
-            </div>
-        </nav>
-    </div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-    <div>asdasdasdasdasdasda</div>
-</div>
-<a class="waves-effect waves-light btn" href="#modal1">Modal</a>
+<!--<div class="row">-->
+<!--    <div class="col s12 main">-->
+<!--        <div class="bg think show"></div>-->
+<!--        <div class="bg enter  "></div>-->
+<!--        <div class="bg-filter"></div>-->
+<!--        <nav>-->
+<!--            <div class="nav-list">-->
+<!--                <div><a href=""><i class="fa fa-user"></i></a></div>-->
+<!--                <div><a href=""><i class="fa fa-desktop"></i></a></div>-->
+<!--                <div><a href=""><i class="fa fa-mouse-pointer"></i></a></div>-->
+<!--                <div><a href=""><i class="fa fa-envelope"></i></a></div>-->
+<!--            </div>-->
+<!--        </nav>-->
+<!--    </div>-->
+<!--</div>-->
+<!--<a class="waves-effect waves-light btn" href="#modal1">Modal</a>-->
+<!---->
+<!--<!-- Modal Structure -->
+<!--<div id="modal1" class="modal bottom-sheet">-->
+<!--    <div class="modal-content">-->
+<!--        <h4>Modal Header</h4>-->
+<!--        <p>A bunch of text</p>-->
+<!--        <p>A bunch of text</p>-->
+<!--        <p>A bunch of text</p><p>A bunch of text</p><p>A bunch of text</p><p>A bunch of text</p><p>A bunch of text</p><p>A bunch of text</p><p>A bunch of text</p><p>A bunch of text</p>-->
+<!--        <p>A bunch of text</p>-->
+<!--        <p>A bunch of text</p>-->
+<!--        <p>A bunch of text</p>-->
+<!--        <p>A bunch of text</p>-->
+<!--        <p>A bunch of text</p>-->
+<!--        <p>A bunch of text</p>-->
+<!--        <p>A bunch of text</p>-->
+<!--        <p>A bunch of text</p>-->
+<!--        <p>A bunch of text</p>-->
+<!--        <p>A bunch of text</p>-->
+<!--        <p>A bunch of text</p>-->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<!--    </div>-->
+<!--    <div class="modal-footer">-->
+<!--        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>-->
+<!--    </div>-->
+<!--</div>-->
 
-<!-- Modal Structure -->
-<div id="modal1" class="modal bottom-sheet">
-    <div class="modal-content">
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
-
+    <div class="home">
+        <div class="bg">
+            <button onclick="$('.bg').height('64px')">click</button>
+        </div>
     </div>
-    <div class="modal-footer">
-        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
-    </div>
-</div>
 @endsection
