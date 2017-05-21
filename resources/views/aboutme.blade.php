0bc5b1487a275cf7a5497df28eda7be75abc5ac4
emilcerezo15 <emil.cerezo15@gmail.com>
2017-05-21 16:37:30 +0800

    Enhancement

--- resources/views/aboutme.blade.php
+++ resources/views/aboutme.blade.php
@@ -1,9 +1,10 @@
 <div>
     <div class="row">
         <div class="col s12">
-            <h4>Lorem ipsum</h4>
+            <h3>Emil Cerezo</h3>
+            <h5>Utilizes skills and abilities in that offers growth</h5>
             <div class="prof-img">
-                <img class="responsive-img circle" src="{{ asset('/images/street.jpg') }}" alt=""/>
+                <div class="emil circle"></div>
             </div>
         </div>
         <div class="col m6">
@@ -13,12 +14,25 @@
                 . Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             </p>
         </div>
-        <div class="col m6">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
-                . Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            </p>
+        <div class="col m6 skills">
+            <div class="col m6">
+                <ul class="">
+                    <li>Javascript | jQuery</li>
+                    <li>CSS3 | SASS</li>
+                    <li>HTML5 | PHP</li>
+                    <li>Socket IO</li>
+                    <li>Photoshop</li>
+                </ul>
+            </div>
+            <div class="col m6">
+                <ul>
+                    <li>SQL</li>
+                    <li>Responsive</li>
+                    <li>Cross-browsing</li>
+                    <li>Email Newslatter</li>
+                    <li>Laravel</li>
+                </ul>
+            </div>
         </div>
         <div class="col m12">
             <p>
